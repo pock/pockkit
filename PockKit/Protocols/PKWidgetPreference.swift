b51a91c988f9260cf47f3df180faedae239c2d69
Pierluigi Galdi <pigi.galdi@gmail.com>
2020-01-12 16:58:54 +0100

    Add PKWidgetPreference protocol

---
+++ PockKit/Protocols/PKWidgetPreference.swift
@@ -0,0 +1,17 @@
+//
+//  PKWidgetPreference.swift
+//  PockKit
+//
+//  Created by Pierluigi Galdi on 11/01/2020.
+//
+
+import Foundation
+import AppKit
+
+/// Pock widget preference protocol.
+@objc (PKWidgetPreferences) public protocol PKWidgetPreference where Self: NSViewController {
+    
+    /// Widget's preference pane nib name.
+    static var nibName: NSNib.Name { get }
+    
+}
